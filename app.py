7fc48666c7269a616b293cbdc6668beee0bb0508
Shashank Singh <cse.shashanksingh@gmail.com>
2018-06-29 20:41:46 +0530

    started main py files

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from url_processor import processURL
+from url_processor import prepareURLfromVidID
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -7,6 +11,17 @@ app = Flask(__name__, static_url_path='/static')
 def main():
 	return render_template('index.html')
 
+@app.route('/submitLink', methods=['POST'])
+def submitLink():
+	vidURL = request.form.get('vidURL')
+	videoID = processURL(vidURL);
+	return redirect(url_for('showLink', vidID=videoID))
+
+@app.route('/showLink', methods=['POST'])
+def showLink():
+	vidID=request.form.get('vidID')
+
+	return render_template('showLink.html', )
 
 if __name__=="__main__":
 	app.run()
---
+++ url_processor.py
@@ -0,0 +1,8 @@
+shareableFolder = 'static/share'
+
+def processURL(vidURL):
+
+
+
+def prepareURLfromVidID(vidID):
+	return '.' + '/' + shareableFolder + '/' + vidID + '.html'
