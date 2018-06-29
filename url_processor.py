9f0f393ca3157d883dae94da283fed9b87b5495b
Shashank Singh <cse.shashanksingh@gmail.com>
2018-06-29 21:06:03 +0530

    testing extraction of meta tags with beautifulsoup

--- url_processor.py
+++ url_processor.py
@@ -2,7 +2,16 @@
 shareableFolder = "static/share"
 
 def processURL(vidURL):
-	return 'a'
+	urlparts = vidURL.split('?');
+	vidID = urlparts[1][2:]
+	# print 'vid id is :' + vidID
+
+	vidImgURL = 'https://img.youtube.com/vi/' + vidID + '.jpg'
+	print 'vid img link is :' + vidImgURL
+
+	
+
+	return '1'
 
 
 
